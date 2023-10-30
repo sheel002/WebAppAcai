a2d80b2545a12b335d029330f1cad11fc28d65a2
Manya <manyamiglani137@gmail.com>
2023-10-30 22:22:54 +0800

    changed the layout of login and register page

--- register.php
+++ register.php
@@ -28,11 +28,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
+<head>
+<meta charset="utf-8">
+    <style>
+    body {font-family:Lato,  sans-serif;
+        background-color: #F8E4E4;
+    }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
 <body>
     <?php if($message): ?>
         <p><?= $message ?></p>
     <?php endif; ?>
 
+    <header>
+        <div class="logo">
+            <img src="Assets/logo.png" alt="Your Logo">
+        </div>
+        <div class="navbar">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="outlets.html">Outlets</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="faq.html">FAQ</a></li>
+                <?php 
+                if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] === false): ?>
+                    <li><a href="login.php" class="nav-item login">Login</a></li>
+                    <li><a href="register.php" class="nav-item register">Register</a></li>
+                <?php else: ?>
+                    <li><a href="logout.php" class="nav-item logout">Logout</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </header>
+    
     <form method="post" action="">
         Name: <input type="text" name="name">
         Email: <input type="email" name="email">
