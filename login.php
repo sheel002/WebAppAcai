abf9b72a27574d48865ae0559cf80d3de4a5ea5c
Manya <manyamiglani137@gmail.com>
2023-10-31 11:49:35 +0800

    final commit before add to cart functionality

--- login.php
+++ login.php
@@ -1,37 +1,15 @@
-<?php
-session_start();
-include 'db_connection.php';
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$message = '';
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $stmt = $conn->prepare("SELECT UserID, Password FROM Users WHERE Email = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $stmt->bind_result($userID, $hashedPassword);
-    $stmt->fetch();
-   
-
-    if ($hashedPassword && password_verify($password, $hashedPassword)) {
-        $_SESSION['UserID'] = $userID;
-        $_SESSION['logged_in'] = true;  // <-- Add this line
-        header("Location: index.php");
-        exit;
-    } else {
-        $message = "Incorrect email or password!";
-    }
-}
-    
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+<head>
+<meta charset="utf-8">
+    <style>
+    body {font-family:Lato,  sans-serif;
+        background-color: #F8E4E4;
+    }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
 <body>
     <header>
         <div class="logo">
@@ -52,10 +30,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <p class="message"><?= $message ?></p>
         <?php endif; ?>
 
-    <form method="post" action="">
-        Email: <input type="email" name="email">
-        Password: <input type="password" name="password">
-        <input type="submit" value="Login">
-    </form>
+        <form method="post" action="">
+            <h2>Login</h2>
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="Login">
+        </form>
+    </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -28,17 +28,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="utf-8">
+    <style>
+        body {
+            font-family: Lato, sans-serif;
+            background-color: #F8E4E4;
+        }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
 <body>
     
     <?php if($message): ?>
         <p><?= $message ?></p>
     <?php endif; ?>
 
-    <form method="post" action="">
-        Name: <input type="text" name="name">
-        Email: <input type="email" name="email">
-        Password: <input type="password" name="password">
-        <input type="submit" value="Register">
-    </form>
+    <header>
+        <div class="logo">
+            <img src="Assets/logo.png" alt="Your Logo">
+        </div>
+        <div class="navbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="outlets.php">Outlets</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="faq.php">FAQ</a></li>
+                <?php 
+                if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] === false): ?>
+                    <li><a href="login.php" class="nav-item login">Login</a></li>
+                    <li><a href="register.php" class="nav-item register">Register</a></li>
+                <?php else: ?>
+                    <li><a href="logout.php" class="nav-item logout">Logout</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </header>
+    
+    <div class="login-container">
+        <form method="post" action="">
+            <h2>Register</h2>
+            <input type="text" name="name" placeholder="Name">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="Register">
+        </form>
 </body>
 </html>
