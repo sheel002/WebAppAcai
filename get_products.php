20ddddcca9bd136491d28463ee36519c630468c8
VidalJemimahRuthDeVera <jemimahvidal@gmail.com>
2023-11-03 21:14:40 +0800

    update

---
+++ get_products.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'db_connection.php';
+
+header('Content-Type: application/json');
+
+
+include 'db_connection.php';
+
+$category = $_GET['category'] ?? '';
+
+$stmt = $conn->prepare("SELECT name, price, image, ingredients FROM products WHERE category = ?");
+$stmt->execute([$category]);
+$result = $stmt->get_result();
+// $products = $result->fetch_all(PDO::FETCH_ASSOC);
+
+// Fetch data
+$products = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $acai_products[] = $row;
+    }
+}
+
+echo json_encode($acai_products);
+
+$conn->close();
+?>
